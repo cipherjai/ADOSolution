596d697f968fa517a6440a42c23892dec1263dba
Jai Gupta <jaiatmart@gmail.com>
2018-09-29 12:28:45 +0530

    SQL Connected Architecture

--- ConnectedConApp/Program.cs
+++ ConnectedConApp/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Data.SqlClient;
+// library is needed to read from config file 
+using System.Configuration;
 
 namespace ConnectedConApp
 {
@@ -12,7 +14,109 @@ namespace ConnectedConApp
   {
     static void Main(string[] args)
     {
+      UseUpdateCustomer();
+      //UseNonQuery();
+      //UseScaler();      
+    }
 
+    private static void UseUpdateCustomer()
+    {
+      SqlConnection northCon = null;
+      SqlCommand customerCMD = null;
+      try
+      {
+        northCon = new SqlConnection(ConfigurationManager.ConnectionStrings["NorthConnection"].ConnectionString);
+
+        SqlParameter[] sqlParameters = new SqlParameter[2];
+        sqlParameters[0] = new SqlParameter("@City", "Panvel");
+        sqlParameters[1] = new SqlParameter("@CustomerId", "JJJ");
+
+
+        customerCMD = new SqlCommand("Update Customers set City=@City where CustomerId=@CustomerId", northCon);
+        customerCMD.Parameters.AddRange(sqlParameters);
+        Console.WriteLine($"customerCMD.CommandText: {customerCMD.CommandText}");
+        Console.ReadKey(true);
+        northCon.Open();
+        int recEffected = customerCMD.ExecuteNonQuery();
+        if(recEffected == 1)
+        {
+          Console.WriteLine("Customers record updated ...");
+        }
+      }
+      catch (Exception ex)
+      {
+
+      }
+      finally
+      {
+        if (northCon.State == System.Data.ConnectionState.Open)
+        {
+          northCon.Close();
+        }
+      }
+      throw new NotImplementedException();
+    }
+
+    // using sql parameters
+
+
+    private static void UseNonQuery()
+    {
+      SqlConnection northCon = null;
+      SqlCommand customerCMD = null;
+      string name = "Jai", city = "country", country = "India";
+      try
+      {
+        northCon = new SqlConnection(ConfigurationManager.ConnectionStrings["NorthConnection"].ConnectionString);
+        string cmdStr = $"Insert into Customers(CustomerId, CompanyName, ContactName, City, Country) values('JJJ', 'FireFly.Inc', 'Jai', 'Mumbai')";
+        customerCMD = new SqlCommand(cmdStr, northCon);
+        Console.WriteLine($"customerCMD CommandText: {customerCMD.CommandText}");
+        northCon.Open();
+        int recEffected = customerCMD.ExecuteNonQuery();
+        if(recEffected == 1)
+        {
+          Console.WriteLine("New Customer Created successfully");
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.ToString());
+      }
+      finally
+      {
+        if (northCon.State == System.Data.ConnectionState.Open)
+        {
+          northCon.Close();
+        }
+      }
+      
+    }
+
+    private static void UseScaler()
+    {
+      SqlConnection northCon = null;
+      SqlCommand productCMD = null;
+      try
+      {
+        northCon = new SqlConnection(ConfigurationManager.ConnectionStrings["NorthConnection"].ConnectionString);
+        productCMD = new SqlCommand("Select Max(UnitPrice) from Products", northCon);
+        northCon.Open();
+        decimal maxPrice = Convert.ToDecimal(productCMD.ExecuteScalar());
+        Console.WriteLine($"Max UnitPrice from Product Table is: {maxPrice}");
+
+      }catch(Exception ex)
+      {
+        Console.WriteLine(ex.ToString());
+      }
+      finally
+      {
+        if (northCon.State == System.Data.ConnectionState.Open)
+        {
+          northCon.Close();
+        }
+      }
+
+      
     }
 
     static void UseReader()
@@ -22,9 +126,9 @@ namespace ConnectedConApp
       SqlCommand productCMD = null;
       try
       {
-        northCon = new SqlConnection(@"Server=(LocalDB)\MSSQLLocalDB; Initial Catalog=Northwind; Integrated Security=true;");
+        //northCon = new SqlConnection(@"Server=(LocalDB)\MSSQLLocalDB; Initial Catalog=Northwind; Integrated Security=true;");
         //northCon = new SqlConnection(@"Server=(LocalDB)\MSSQLLocalDB; Initial Catalog=Northwind; Integrated Security=true;MultipleActiveResultSets=true;");
-
+        northCon = new SqlConnection(ConfigurationManager.ConnectionStrings["NorthConnection"].ConnectionString);
         #region Category
         categoryCMD = new SqlCommand("Select * from Categories", northCon);
         northCon.Open();
